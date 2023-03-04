71984583db4fc04d306ca998cbf4b7195b7012d7
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-04 16:19:18 +0100

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,14 +1,19 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema(
-    {
-        name:{type :String, require : true, default :""},
-        age:Number,
-        moy:Number,
-        address:{
-            street:String,
-            country:String
-        }
-    },{timestamps:true}
-);
-const user = mongoose.model("user", userSchema);
-module.exports=user;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  dateOfBirth: { type: Date, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+  userType: { type: String, enum: ['admin', 'regular'], required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
