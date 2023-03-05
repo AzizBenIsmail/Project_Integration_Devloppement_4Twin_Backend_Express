8ae2dbab15e32d581f58f8521035d3953a81c662
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-05 18:47:02 +0100

    optimisation du schema et middlewares

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String,unique: true },
-  first_Name: { type: String },
-  last_Name: { type: String },
-  email: { type: String, unique: true },
-  password: { type: String },
-  created_at: { type: Date }, //2017-01-01 type dentre dans postman
-  updated_at: { type: Date, }, //2017-01-01 type dentre dans postman
-  dateOfBirth: { type: Date }, //2017-01-01 type dentre dans postman
-  phoneNumber: { type: Number },
-  gender: { type: String },
-  userType: { type: String },
-  address: { type: String },
+  username: String, //unique
+  first_Name: String , //min4 max 10
+  last_Name: String , //min4 max 10
+  email: String , //unique + email valid
+  password: String , //8 char /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+  created_at: Date , //2017-01-01 type dentre dans postman
+  //updated_at: { type: Date, required: false, default: null }, method: 1 
+  updated_at: Date, //method: 2
+  dateOfBirth: Date , //You must be at least 18 years old
+  phoneNumber: Number , //length 8
+  gender: String , //oneof (['Male', 'Female', 'Other'])
+  userType: String , //oneof (['admin', 'regular', 'fablab'])
+  address: String , //min 5 max 15
 });
 
 const User = mongoose.model('User', userSchema);
