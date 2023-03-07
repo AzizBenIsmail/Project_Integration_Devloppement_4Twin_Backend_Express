302af46ed18443939209408c3c5ca121ab26539c
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-07 13:07:39 +0100

    update (NotRequired)

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -10,29 +10,29 @@ const validate = async (req, res, next) => {
                 const isUnique = await checkusernameUniqueness(value);
                 return isUnique;
             }),
-            first_Name: yup.string().required()
+            first_Name: yup.string().notRequired()
                 .min(4, 'Username must contain at least 4 characters').max(10, 'The first_Name must contain a maximum of 10 characters'),
-            last_Name: yup.string().required()
+            last_Name: yup.string().notRequired()
                 .min(4, 'Username must contain at least 4 characters').max(10, 'The last_Name must contain a maximum of 10 characters'),
-            email: yup.string().required()
+            email: yup.string().Required()
                 .email().test('email_unique', 'email is already taken', async function (value) {
                     const isUnique = await checkEmailUniqueness(value);
                     return isUnique;
                 }),
-            password: yup.string().required()
+            password: yup.string().notRequired()
                 .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, 'password : a character string of at least 8 characters containing at least one letter and one number'),
             created_at: yup.date().notRequired(),
             //updated_at: yup.date().notRequired(), methode 1 
             updated_at: yup.date().notRequired().default(null),   //methode 2
             dateOfBirth: yup.date().required()
                 .max(new Date(Date.now() - (18 * 365 * 24 * 60 * 60 * 1000)), 'You must be at least 18 years old'),
-            phoneNumber: yup.number()
+            phoneNumber: yup.number().notRequired()
                 .typeError('The phone number must be a number').test('len', 'The phone number must contain 8 digits', val => val.toString().length === 8),
-            gender: yup.string()
+            gender: yup.string().notRequired()
                 .oneOf(['Male', 'Female', 'Other']),
-            userType: yup.string()
+            userType: yup.string().notRequired()
                 .oneOf(['admin', 'regular', 'fablab']),
-            address: yup.string()
+            address: yup.string().notRequired()
                 .min(5, 'address must contain at least 5 characters').max(15, 'address must contain at least 15 characters')
         });
         async function checkusernameUniqueness(username) {
