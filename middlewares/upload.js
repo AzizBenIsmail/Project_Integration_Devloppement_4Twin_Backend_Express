97b9adccd8030a8273e1dce80342c502668ec90e
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-09 17:23:34 +0100

    problème image résolu

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -4,6 +4,7 @@ var storage = multer.diskStorage({
     cb(null, 'public/images')
   },
   filename: function (req, file, cb) {
+     console.log('file',file);
 //     const {username}=file.originalname;
 // console.log('file uploade',file.);
 //     cb(null, username + '.png')
