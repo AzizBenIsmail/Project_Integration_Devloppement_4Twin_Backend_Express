ced6fbca57061dfa58aea5c01c9660758982e7cf
Oumaymatouil <96637712+Oumaymatouil@users.noreply.github.com>
2023-03-14 00:15:57 +0100

    Update fablabResquestSchema.js

--- models/fablabResquestSchema.js
+++ models/fablabResquestSchema.js
@@ -3,11 +3,11 @@ const { boolean } = require('yup');
 const Schema = mongoose.Schema;
 
 const fablabRequestSchema = new Schema({
-  fablabName: String, //unique
-  fablabEmail: String, //unique + email valid
-  dateOfCreation: Date, //You must be at least 18 years old
-  phoneNumber: Number, //length 8
-  address: String, //min 5 max 15
+  fablabName: String, 
+  fablabEmail: String, 
+  dateOfCreation: Date, 
+  phoneNumber: String, 
+  address: String, 
   fablbLogo: String,
   description : String,
   is_treated : Boolean,
