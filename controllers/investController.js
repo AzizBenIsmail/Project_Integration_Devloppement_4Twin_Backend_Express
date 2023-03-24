84157be2a2aeb8748d84eb741e711e924f8e2d08
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-24 01:59:50 +0100

    crud investir complet + fonctionnel
    tester par Postman

---
+++ controllers/investController.js
@@ -0,0 +1,85 @@
+const projectModel = require("../models/projectSchema");
+const userModel = require("../models/userSchema");
+const investModel = require("../models/investSchema");
+
+const addInvest = async (req, res, next) => {
+    try {
+      const { idUser,idProject } = req.params;
+      const {message,montant} = req.body;
+      created_at = new Date();
+      const user = await userModel.findById(idUser); 
+      const project = await projectModel.findById(idProject); 
+      const invest = new investModel({
+        message,montant,investor: user,project: project
+      });
+      
+      const addedInvest = await invest.save().then((savedInvest) => {
+        // find the user by ID
+        userModel.findById(idUser).then((user) => {
+            // add the project ID to the user's project array
+            user.invests.push(savedInvest._id);
+            // save the user to the database
+            user.save().then(() => {
+                console.log('invest added to user successfully');
+              }).catch((err) => {
+                console.error(err);
+              });
+          }).catch((err) => {
+            console.error(err);
+          });
+          // find the project by ID
+        projectModel.findById(idProject).then((project) => {
+            // add the project ID to the user's project array
+            project.invests.push(savedInvest._id);
+            // save the user to the database
+            project.save().then(() => {
+                console.log('invest added to Project successfully');
+              }).catch((err) => {
+                console.error(err);
+              });
+          }).catch((err) => {
+            console.error(err);
+          });
+      }).catch((err) => {
+        console.error(err);
+      });
+      res.status(200).json(project);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  const getInvest = async (req, res, next) => {  
+    try {
+      const invests = await investModel.find();
+      if (!invests || invests.length === 0) {
+        throw new Error("invests not found !");
+      }
+      res.status(200).json({ invests });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  const deleteInvest = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const invest = await investModel.findById(id); 
+  
+      if (!invest) {
+        return res.status(404).json({ message: "invest not found!" });
+      }
+  
+      await invest.remove();
+      res.status(200).json({ message: "invest deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  module.exports = {
+    addInvest,
+    deleteInvest,
+    getInvest
+  };
+  
--- models/investSchema.js
+++ models/investSchema.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const userModel = require("../models/userSchema");
+const projectModel = require("../models/projectSchema");
+
+
 const investSchema = new Schema({
   _id: {
     type: mongoose.Schema.Types.ObjectId,
@@ -7,7 +11,7 @@ const investSchema = new Schema({
   },
   message: String, 
   created_at: Date, //2017-01-01 type dentre dans postman
-  montant: number, 
+  montant: Number, 
   investor: {                //it is a many to one relationship a Invest can have one Investor (User)
     type: mongoose.Schema.Types.ObjectId,   
     ref: 'User',
@@ -20,6 +24,24 @@ const investSchema = new Schema({
   },
 });
 
+investSchema.pre('remove', async function(next) {
+  try {
+    const invest = this;
+    const user = await userModel.findOne({ invests: invest._id });
+    if (user) {
+      user.invests = user.invests.filter(id => id.toString() !== invest._id.toString());
+      await user.save();
+    }
+    const project = await projectModel.findOne({ invests: invest._id });
+    if (project) {
+      project.invests = project.invests.filter(id => id.toString() !== invest._id.toString());
+      await project.save();
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 const Invest = mongoose.model("Invest", investSchema);
 
 module.exports = Invest;
