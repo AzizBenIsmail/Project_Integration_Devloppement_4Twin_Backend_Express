e501a6a16bfb4be73f7834600a979a52b3f01304
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-05-04 14:42:59 +0100

    UI chat

--- controllers/Chat/informationsController.js
+++ controllers/Chat/informationsController.js
@@ -1,7 +1,41 @@
 const projectModel = require('../../models/projectSchema');
 const userModel = require('../../models/userSchema');
+
+
+const perspective = require('perspective-api-client');
+
+
+
 const projectInfo = async (req, res, next) => {
     try {
+
+
+      const client = new perspective({
+        apiKey: 'AIzaSyD8EsP6LrDD5wsHHLPaN6SP_22cvKXTNE0',
+      });
+      
+      const text = 'you are badword';
+      
+      client.analyze({
+        comment: { text },
+        languages: ['en'],
+        requestedAttributes: {
+          TOXICITY: {},
+        },
+      })
+      .then((response) => {
+        const toxicityScore = response.attributeScores.TOXICITY.summaryScore.value;
+        if (toxicityScore > 0.5) {
+          console.log('Harassment detected!');
+        } else {
+          console.log('No harassment detected.');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+
         const id=await userId(req.body.username )
       const projects = await projectModel
         .find({ creator: await id})
@@ -15,6 +49,26 @@ const projectInfo = async (req, res, next) => {
       res.status(500).json({ message: error.message });
     }
   };
+
+
+  const jobOffer = async (req, res, next) => {
+    try {
+
+        const id=await userId(req.body.username)
+      const projects = await projectModel
+        .find({ creator: await id})
+        .populate("creator");
+      console.log(projects);
+      if (!projects || projects.length === 0) {
+        throw new Error("No projects found for this creator.");
+      }
+      res.status(200).json({ projects });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
 
 
   const userId = async (username) => {
